a24ee8d125e9834bbc8500a5c354df88ffb1294c
jakecoltman <jakecoltman@sky.com>
2016-04-02 10:40:16 +0100

    Added orelse parser combiner

--- LookMLParser/LookMLParser/Program.fs
+++ LookMLParser/LookMLParser/Program.fs
@@ -57,15 +57,34 @@ let andThen parser1 parser2 =
 
     Parser innerFn
 
+let orElse parser1 parser2 = 
+    let innerFn input = 
+        let result1 = run parser1 input
+
+        match result1 with 
+            | Failure err ->
+                let result2 = run parser2 input
+                match result2 with 
+                | Failure err -> 
+                    Failure err
+                | Success (value2, remaining2) ->
+                    Success (value2, remaining2)
+
+            | Success (value1, remaining1) -> 
+                Success (value1, remaining1)
+
+    Parser innerFn
+
 let ( .>>. ) = andThen
+let ( .>>>. ) = orElse
 let parseD = parse_character 'D'
 let parseE = parse_character 'E'
 
 let parseDthenE = parseD .>>. parseE
-
+let parseDorE = parseD .>>>. parseE
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" (run parseDthenE  "DEllo world")
+    printfn "%A" (run parseDorE  "EDEllo world")
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
