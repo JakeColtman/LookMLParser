73c7e82a9756ba1279482f12bc63b13a8da606ed
jakecoltman <jakecoltman@sky.com>
2016-04-02 09:53:24 +0100

    Added ability to chain parsers together

--- LookMLParser/LookMLParser/Program.fs
+++ LookMLParser/LookMLParser/Program.fs
@@ -1,7 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open System
+
+let Parser str = 
+    if String.IsNullOrEmpty(str) then
+        (false , "")
+    else if str.[0] = 'A' then 
+        (true, str.[1..])
+    else
+        (false, str)
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    printfn "%A" (Parser "AJake")
+    System.Console.ReadKey() |> ignore
     0 // return an integer exit code
