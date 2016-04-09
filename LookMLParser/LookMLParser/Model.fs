253605321d2db7b60ae683c0af531efe234730bc
jakecoltman <jakecoltman@sky.com>
2016-04-09 10:40:28 +0100

    Type modelling for fields

---
+++ LookMLParser/LookMLParser/Model.fs
@@ -0,0 +1,48 @@
+﻿namespace LookMLParser 
+
+module Model = 
+
+    type DimensionType = 
+        | String
+        | Number
+        | YesNo
+        | Time
+        | Tier
+        | Location
+        | ZipCode
+
+    type MetricType = 
+        | String 
+        | Date
+        | Number
+        | Count
+        | CountDistinct
+        | Sum
+        | SumDistinct
+        | Avg
+        | AvgDistinct
+        | Min
+        | Max
+        | List
+        | PercentOfPrevious
+        | PercentOfTotal
+        | RunningTotal
+
+    type DimensionGroup = {
+        ttype : DimensionType;
+        hidden : bool;
+        primary_key: bool;
+        sql: string
+    }
+
+    type Dimension = {
+        ttype : DimensionType;
+        hidden : bool;
+        primary_key: bool;
+        sql: string
+    }
+
+    type Field = 
+        | Dimension
+        | Metric
+        | DimensionGroup
