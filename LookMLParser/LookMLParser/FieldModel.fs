876b77577adedccb262f72deace3443ff95cc96a
jakecoltman <jakecoltman@sky.com>
2016-04-12 07:58:46 +0100

    Moved field model into own file
    Fairly overweight to be in one file with others

---
+++ LookMLParser/LookMLParser/FieldModel.fs
@@ -0,0 +1,98 @@
+﻿namespace LookMLParser 
+
+
+module FieldModel = 
+
+    type DimensionStyle = 
+        | Interval
+        | Classic 
+        | Integer
+        | Relational
+
+    type MeasureDirection = 
+        | Row
+        | Column
+
+    type DimensionDataType = 
+        | String
+        | Number
+        | YesNo
+        | Time
+        | Tier
+        | Location
+        | ZipCode
+
+    type DimensionGroupDataType = 
+        | Epoch
+        | TimeStamp
+        | DateTime
+        | Date
+        | YYYYMMDD
+
+    type MeasureDataType = 
+        | String 
+        | Date
+        | Number
+        | Count
+        | CountDistinct
+        | Sum
+        | SumDistinct
+        | Avg
+        | AvgDistinct
+        | Min
+        | Max
+        | List
+        | PercentOfPrevious
+        | PercentOfTotal
+        | RunningTotal
+
+    type LookerDataType = 
+        | MeasureDataType of MeasureDataType
+        | DimensionDataType of DimensionDataType
+
+    type DimensionGroupDetails = {
+        data_type : DimensionGroupDataType;
+        convert_tz : bool;
+        primary_key: bool;
+        timeframes : list<string>
+    }
+
+    type DimensionDetails = {
+        data_type : DimensionDataType;
+        primary_key: bool;
+        alpha_sort: bool;
+        tiers : Option<list<int>>;
+        style: Option<DimensionStyle>;
+        suggestable: bool
+    }
+
+    type MeasureDetails = {
+        data_type : MeasureDataType;
+        direction: Option<MeasureDirection>;
+        approximate: Option<bool>;
+        approximate_threshold : Option<int>;
+        sql_distinct_key : Option<string>;
+        list_field: Option<string>;
+        filters: Option<string>
+    }
+
+    type FieldDetails = {
+        sql: string;
+        label : Option<string>;
+        view_label: Option<string>;
+        description: Option<string>;
+        hidden: bool;
+        alias: Option<string>;
+        required_fields: Option<string>;
+        drill_fields: Option<string>
+    }
+
+    type FieldType = 
+        | DimensionType
+        | MeasureType
+        | DimensionGroupType
+
+    type Field = 
+        | Dimension of FieldType * DimensionDetails * FieldDetails
+        | Measure of FieldType * MeasureDetails * FieldDetails
+        | DimensionGroup of FieldType * DimensionGroupDetails * FieldDetails
