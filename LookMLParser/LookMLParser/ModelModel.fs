018e179499dfb7f6c45a72f944f6e480f4751acc
jakecoltman <jakecoltman@sky.com>
2016-04-14 08:22:17 +0100

    Added types for looker model
    Not as complex as the field definitions

---
+++ LookMLParser/LookMLParser/ModelModel.fs
@@ -0,0 +1,37 @@
+﻿namespace LookMLParser 
+
+module ModelModel = 
+
+    type Relationship = 
+        | OneToOne
+        | ManyToOne
+        | OneToMany
+        | ManyToMany
+    
+    type JoinType = 
+        | InnerJoin
+        | FullOuterJoin
+        | CrossJoin
+        | LeftJoin
+
+    type Join = {
+        join: string;
+        relationship: Relationship;
+        sql_on : string;
+        join_type: JoinType;
+        view_label: string
+    }
+
+    type Explore = {
+        explore: string;
+        label: string;
+        view: string;
+        fields: string list; 
+        joins: Join list    
+    }
+
+    type Model =  {
+        includes: string list;
+        connection: string;
+        explores: Explore list    
+    }
