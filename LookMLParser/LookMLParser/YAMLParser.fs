85edd8808665192221e953e12da3d59d12764410
jakecoltman <jakecoltman@sky.com>
2016-04-15 19:27:21 +0100

    Added parsers for key value pair and sequence

--- LookMLParser/LookMLParser/YAMLParser.fs
+++ LookMLParser/LookMLParser/YAMLParser.fs
@@ -1,6 +1,17 @@
 ﻿namespace LookMLParser 
 
 module YAMLParser = 
+    open BasicParser;
+
+    let dash = parse_character '-'
+    let colon = parse_character ':'
+    let character : Parser<char> = anyOf [ 'a'.. 'z']
+    let extended_characters = anyOf (List.append [ 'a'.. 'Z'] [ '$'; '{'; '}'; '_'; '.'] )
+    let string = many character
+    let extendedString = many (character <|> extended_characters)
+    let single_whitespace : Parser<char> = anyOf [ ' '  ; '\t' ]
+    let line_break : Parser<char> = anyOf [ ' '  ; '\n' ; '\r' ; '\t' ]
+    let whitespace = many single_whitespace
 
     type Scalar = string
 
@@ -12,5 +23,12 @@ module YAMLParser =
         | Scalar of Scalar
         | Sequence of Sequence
         | Mapping of Mapping
+
+    let p_keyValuePair = 
+        extendedString .>> colon .>>. extendedString
+
+    let sequenceEntry = 
+        dash >>. whitespace >>. extendedString
+
         
         
