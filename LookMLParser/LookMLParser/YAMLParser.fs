77f8f209c84dec4fe42b779d3814ce1b4ecb607b
jakecoltman <jakecoltman@sky.com>
2016-04-15 19:22:30 +0100

    Added YAML parser
    First level of parsing Lookml

---
+++ LookMLParser/LookMLParser/YAMLParser.fs
@@ -0,0 +1,16 @@
+﻿namespace LookMLParser 
+
+module YAMLParser = 
+
+    type Scalar = string
+
+    type Sequence = string list
+    
+    type Mapping = string list
+    
+    type Node = 
+        | Scalar of Scalar
+        | Sequence of Sequence
+        | Mapping of Mapping
+        
+        
