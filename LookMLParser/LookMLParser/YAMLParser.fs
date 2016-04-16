dd5484094089a8d254c39bf42cac1e0c8bce1ac3
jakecoltman <jakecoltman@sky.com>
2016-04-16 07:34:23 +0100

    Minor bug fixes to make work

--- LookMLParser/LookMLParser/YAMLParser.fs
+++ LookMLParser/LookMLParser/YAMLParser.fs
@@ -10,7 +10,7 @@ module YAMLParser =
     let string = many character
     let extendedString = many (character <|> extended_characters) |>>  ( fun char_list -> charListToString char_list)
     let single_whitespace : Parser<char> = anyOf [ ' '  ; '\t' ]
-    let line_break : Parser<char> = anyOf [ ' '  ; '\n' ; '\r' ; '\t' ]
+    let endOfLineParser = string_parser "\r\n"
     let whitespace = many single_whitespace
     let parser_to_output_map input = 
         input |> Map.ofList
@@ -21,21 +21,21 @@ module YAMLParser =
         | Mapping of Map<string, string>
 
     let p_keyValuePair indentation = 
-        indentation >>. extendedString .>> colon .>> whitespace .>>. extendedString
+        indentation >>. extendedString .>> colon .>> whitespace .>>. extendedString .>> endOfLineParser
 
     let p_mapping indentation = 
         (many1 (p_keyValuePair indentation)) |>> (fun x -> Mapping (parser_to_output_map x))
 
     let p_sequenceEntry indentation = 
-        indentation >>. dash .>> whitespace >>. extendedString
+        indentation >>. dash .>> whitespace >>. extendedString .>> endOfLineParser
 
     let p_sequence indentation = 
         (many1 (p_sequenceEntry indentation)) |>> (fun x -> Sequence(x))
 
-    let p_scalar indentation = indentation >>. extendedString |>> (fun x -> Scalar(x))
+    let p_scalar indentation = indentation >>. extendedString .>> endOfLineParser |>> (fun x -> Scalar(x))
 
     let p_node indentation_level = 
         let indentation = manyN single_whitespace indentation_level 
-        (p_sequence indentation) //<|> (p_mapping indentation) <|> (p_scalar indentation)
+        (p_sequence indentation) <|> (p_mapping indentation) <|> (p_scalar indentation)
         
     let p_nodes = many1 (p_node 2)
