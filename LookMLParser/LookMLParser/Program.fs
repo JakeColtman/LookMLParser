5af0b67a89bcbee86b86cce5c1783e1d25eae594
jakecoltman <jakecoltman@sky.com>
2016-06-11 11:25:30 +0100

    Added parser support for mapping list of maps
    Closer to how lookml actually is

--- LookMLParser/LookMLParser/Program.fs
+++ LookMLParser/LookMLParser/Program.fs
@@ -3,9 +3,21 @@
 
 open FsYaml;
 open System.IO;
-open LookMLParser.View;
 
-type view = {view: string;  derived_table: string; fields: string; sets: string}
+
+type sets = {sets: string}
+type fields = {fields: string}
+type derived_table = {derived_table: string}
+type view = {view: string}
+
+type highest_level = 
+    | Set of sets
+    | Field of fields
+    | Derived_Table of derived_table
+    | View of view
+
+
+type lookml = Map<string, string> list
 
 [<EntryPoint>]
 let main argv =
@@ -14,7 +26,7 @@ let main argv =
     let looker_location = @"E:\looker\test.view.lookml"
     
     let string_yaml = File.ReadAllText looker_location
-    let x = Yaml.load<view> string_yaml
+    let x = Yaml.load<lookml> string_yaml
 
     printfn "%A" x
     System.Console.ReadKey() |> ignore
