8a2af462a4a9ddee27b7e2fa4ae952b61c34dcdc
jakecoltman <jakecoltman@sky.com>
2016-06-11 12:38:22 +0100

    Added integration layer for sets
    Takes a json object and safely converts it into the domain model.

---
+++ LookMLParser/LookMLParser/IntegrationLayer.fs
@@ -0,0 +1,27 @@
+﻿namespace LookMLParser
+
+module IntegrationLayer = 
+    open LookMLParser.FieldModel;
+    open LookMLParser.SetModel;
+    open LookMLParser.View;
+    open LookMLParser.DataSource;
+    open FSharp.Data.JsonExtensions;
+    open FSharp.Data;
+   
+
+    let process_set json_set = 
+        let name = fst json_set
+        let entries = snd json_set
+        let mutable columns = []
+        match entries with 
+            | JsonValue.Array a ->
+                for entry in a do
+                    columns <- (entry.AsString())::columns
+            | _ -> printfn "%A" "nothing"
+        {Set.name = name; fields = columns}
+
+    let parse_sets json_contents = 
+        match json_contents with 
+            | JsonValue.Record x -> 
+                Some (Array.map process_set x)
+            | _ -> None
--- LookMLParser/LookMLParser/Program.fs
+++ LookMLParser/LookMLParser/Program.fs
@@ -1,16 +1,8 @@
 ﻿open System.IO
 open FSharp.Data
-
 open FSharp.Data.JsonExtensions
-
-type sets = Map<string, string>
-type fields = {fields: string}
-type derived_table = {derived_table: string}
-type view = {view: string}
-
-type highest_level = 
-    | Set of sets
-    | String
+open LookMLParser.IntegrationLayer
+open LookMLParser.SetModel;
 
 type lookml = Map<string, obj> list
 
@@ -21,8 +13,19 @@ let main argv =
     let looker_location = @"E:\looker\testJson.view.lookml"
     
     let string_yaml = File.ReadAllText looker_location
-    let x = JsonValue.Parse(string_yaml)
+    let view_json = JsonValue.Parse(string_yaml)
     
-    printfn "%A" (x?view.AsString())
+    let view = view_json?view
+    let fields = view_json?fields
+    let derived_table = view_json?derived_table
+    let sets = view_json?sets
+
+    let parsed_sets = parse_sets sets
+    match parsed_sets with 
+        | Some sets -> 
+           Array.iter (fun (s:Set) -> printfn "%A" s.name) sets
+        | None -> printfn "%A" "Failure"
+
+
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
