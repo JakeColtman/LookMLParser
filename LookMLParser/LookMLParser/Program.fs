1201d8f6d86c85d640ea43ffe23b82ba37e2e6dd
jakecoltman <jakecoltman@sky.com>
2016-06-11 11:17:46 +0100

    Parsed the simplest dummy version
    Only a mapping of string to string

--- LookMLParser/LookMLParser/Program.fs
+++ LookMLParser/LookMLParser/Program.fs
@@ -1,16 +1,21 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-open YamlDotNet.Serialization;
-open YamlDotNet.Serialization.NamingConventions;
+open FsYaml;
 open System.IO;
+open LookMLParser.View;
+
+type view = {view: string;  derived_table: string; fields: string; sets: string}
 
 [<EntryPoint>]
 let main argv =
+
+
     let looker_location = @"E:\looker\test.view.lookml"
     
     let string_yaml = File.ReadAllText looker_location
+    let x = Yaml.load<view> string_yaml
 
-    printfn "%A" string_yaml
+    printfn "%A" x
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
