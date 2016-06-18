01a9f346a324582ab32d4a42d5ff4951d1d5dd3a
jakecoltman <jakecoltman@sky.com>
2016-06-18 10:10:50 +0100

    Extracted config into sep files
    Avoids clutter and easier to have multiple config setups.  Going to make
    this a command line flag

---
+++ Ecce/configurations.py
@@ -0,0 +1,30 @@
+from Ecce.Validation.all_fields_exist_in_sql import all_fields_exist_in_sql
+from Ecce.Validation.meets_naming_convention import all_fields_obey_naming_convention
+from Ecce.Validation.no_select_alls_in_sql_query import no_select_all
+from Ecce.Processing.remove_bad_set_points import remove_set_elements_that_have_no_views
+
+configs = {
+
+    "strictest": {
+        "validation": [
+            all_fields_exist_in_sql,
+            all_fields_obey_naming_convention,
+            no_select_all
+        ],
+        "processing": [
+            remove_set_elements_that_have_no_views
+        ]
+
+    },
+
+    "only_errors": {
+        "validation": [
+            all_fields_exist_in_sql
+        ],
+        "processing": [
+        ]
+
+    }
+
+
+}
--- Ecce/deploy.py
+++ Ecce/deploy.py
@@ -1,8 +1,6 @@
 from Ecce.json_to_lookml import print_from_json
-from Ecce.Validation.all_fields_exist_in_sql import all_fields_exist_in_sql
-from Ecce.Validation.meets_naming_convention import all_fields_obey_naming_convention
-from Ecce.Validation.no_select_alls_in_sql_query import no_select_all
 import yaml
+from configurations import configs
 
 def load_from_yaml(file_name):
     with open(file_name, "r") as file_open:
@@ -11,15 +9,8 @@ def load_from_yaml(file_name):
 
 view = load_from_yaml("E:/looker/seller_debtors.view.lookml")
 
-validating_functions = [
-    all_fields_exist_in_sql,
-    all_fields_obey_naming_convention
-    #no_select_all
-]
-
-post_processing_functions = [
-
-]
+config = configs["only_errors"]
+validating_functions, post_processing_functions = config["validation"], config["processing"]
 
 for validating_function in validating_functions:
     if not validating_function(view):
