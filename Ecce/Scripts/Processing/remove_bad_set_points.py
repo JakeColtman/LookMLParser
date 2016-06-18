f91aacd68d4e5ff28576c8a7c40a1f9dc97ba9ae
jakecoltman <jakecoltman@sky.com>
2016-06-18 09:19:03 +0100

    Spruced up
    Doesn't need to go through the loop so many times.
    
    Can make use of helper functions
    
    Need to add in some kind of deployment logging

---
+++ Ecce/Scripts/Processing/remove_bad_set_points.py
@@ -0,0 +1,16 @@
+from ..Helper.Field import fields_exposed_to_sets
+
+def remove_set_elements_that_have_no_views(view):
+    if "sets" not in view:
+        return []
+
+    sets = view["sets"]
+    exposed_field_names = fields_exposed_to_sets(view)
+
+    for set_name in sets:
+        for set_entry in sets[set_name]:
+            if set_entry not in exposed_field_names and "*" not in set_entry and "." not in set_entry:
+                sets[set_name].pop(set_entry)
+    return view
+
+
