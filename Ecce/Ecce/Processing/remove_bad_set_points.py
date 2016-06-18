87f7cf2a6c7d8e08485c759eaac5a149906994fd
jakecoltman <jakecoltman@sky.com>
2016-06-18 10:24:37 +0100

    Bug fix - list not dict

--- Ecce/Ecce/Processing/remove_bad_set_points.py
+++ Ecce/Ecce/Processing/remove_bad_set_points.py
@@ -10,7 +10,7 @@ def remove_set_elements_that_have_no_views(view):
     for set_name in sets:
         for set_entry in sets[set_name]:
             if set_entry not in exposed_field_names and "*" not in set_entry and "." not in set_entry:
-                sets[set_name].pop(set_entry)
+                sets[set_name].remove(set_entry)
     return view
 
 
